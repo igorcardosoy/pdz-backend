7f7c0526e2801473e401fff34de37dafb85e00cb
igorcardosoy <igorfilipicardoso@gmail.com>
2026-05-12 14:21:37 -0300

    feat: restrict access to VideoDownloaderController to USER, MODERATOR, and ADMIN roles

--- src/main/java/br/app/pdz/api/controller/VideoDownloaderController.java
+++ src/main/java/br/app/pdz/api/controller/VideoDownloaderController.java
@@ -6,11 +6,13 @@ import br.app.pdz.api.service.VideoDownloaderService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/pdz-api/videos")
+@PreAuthorize("hasAnyRole('USER', 'MODERATOR', 'ADMIN')")
 public class VideoDownloaderController {
 
     private final VideoDownloaderService videoDownloaderService;
